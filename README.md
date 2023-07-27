12663e1896f7855c90541faaf47ca41548c78b31
MirianRuanova <mruanova.us@gmail.com>
2023-07-26 17:58:00 -0700

    Improving the visualization

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # belly-button-challenge
+
+**Belly Button Biodiversity Dashboard**
+
+The Belly Button Biodiversity Dashboard is an interactive web application that allows users to explore and visualize the biodiversity of belly button microbes for different test subjects. The data used in the dashboard is provided in a JSON format and reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+**Features:**
+
+1. Dropdown Select: The dashboard provides a dropdown menu that allows users to select a test subject ID number. When a new test subject is selected, the dashboard updates all the plots and demographic information accordingly.
+
+2. Bar Chart: The bar chart displays the top 10 bacteria strains found in the selected test subject's belly button. The bars are sorted in descending order, with the most prominent strains shown first.
+
+3. Bubble Chart: The bubble chart displays all the bacteria strains found in the selected test subject's belly button. The size of each bubble represents the quantity of the respective bacteria, and the color represents the OTU ID of the bacteria.
+
+4. Gauge Chart: The gauge chart displays the washing frequency of the selected test subject's belly button. The gauge chart helps users understand how frequently the test subject washes their belly button, measured in scrubs per week.
+
+5. Demographic Info: The dashboard displays demographic information about the selected test subject, including their age, gender, ethnicity, and belly button type. The information is dynamically updated when a new test subject is selected.
+
+**Technologies Used:**
+
+The Belly Button Biodiversity Dashboard is built using HTML, CSS (via Bootstrap), and JavaScript. The JavaScript libraries D3.js and Plotly.js are used for data manipulation and visualization. The JSON data containing the microbial samples is fetched using D3.json and processed to update the charts and demographic information.
+
+**How to Use:**
+
+To use the Belly Button Biodiversity Dashboard, simply open the HTML file in a web browser. Upon loading, the dashboard will display the bar chart, bubble chart, and gauge chart for the first test subject. Use the dropdown menu labeled "Test Subject ID No." to select a different test subject. The dashboard will update all the visualizations and demographic information based on the selected test subject.
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,126 +1,182 @@
 // Function to read samples.json using D3
 function readSamplesJSON() {
-    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-    d3.json(url)
-      .then(function (data) {
-        const testSubjectIds = data.names;
-        const dropdown = d3.select("#selDataset");
-        testSubjectIds.forEach(subjectId => {
-          dropdown.append("option").text(subjectId).property("value", subjectId);
-        });
-        optionChanged(testSubjectIds[0], data);
-        dropdown.on("change", function () {
-          const selectedSubjectId = dropdown.property("value");
-          optionChanged(selectedSubjectId, data);
-        });
-      })
-      .catch(function (error) {
-        console.log("Error loading data:", error);
-      });
-  }
-  
-  // Call the function to read samples.json
-  readSamplesJSON();
-  
-  // Function to create the horizontal bar chart
-  function createBarChart(selectedSubjectId, data) {
-    const selectedData = data.samples.find(sample => sample.id === selectedSubjectId);
-    const top10SampleValues = selectedData.sample_values.slice(0, 10).reverse();
-    const top10OtuIds = selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
-    const top10OtuLabels = selectedData.otu_labels.slice(0, 10).reverse();
-    const trace = {
-      x: top10SampleValues,
-      y: top10OtuIds,
-      text: top10OtuLabels,
-      type: "bar",
-      orientation: "h"
-    };
-    const layout = {
-      title: `Top 10 OTUs Found in Test Subject ${selectedSubjectId}`,
-      xaxis: { title: "Sample Values" },
-      yaxis: { title: "OTU IDs" }
-    };
-    Plotly.newPlot("bar", [trace], layout);
-  }
-  
-  // Function to create the bubble chart
-  function createBubbleChart(selectedSubjectId, data) {
-    const selectedData = data.samples.find(sample => sample.id === selectedSubjectId);
-    const trace = {
-      x: selectedData.otu_ids,
-      y: selectedData.sample_values,
-      text: selectedData.otu_labels,
-      mode: "markers",
-      marker: {
-        size: selectedData.sample_values,
-        color: selectedData.otu_ids,
-        colorscale: "Earth"
-      }
-    };
-    const layout = {
-      title: `Sample Values for Test Subject ${selectedSubjectId}`,
-      xaxis: { title: "OTU IDs" },
-      yaxis: { title: "Sample Values" }
-    };
-    Plotly.newPlot("bubble", [trace], layout);
-  }
-  
-  // Function to create the gauge chart
-  function createGaugeChart(washingFrequency) {
-    const data = [
-      {
-        domain: { x: [0, 1], y: [0, 1] },
-        value: washingFrequency,
-        title: { text: "Weekly Washing Frequency" },
-        type: "indicator",
-        mode: "gauge+number",
-        gauge: {
-          axis: { range: [null, 9] },
-          bar: { color: "#000" },
-          steps: [
-            { range: [0, 1], color: "#f7f7f7" },
-            { range: [1, 2], color: "#f0f0f0" },
-            { range: [2, 3], color: "#e8e8e8" },
-            { range: [3, 4], color: "#e0e0e0" },
-            { range: [4, 5], color: "#d8d8d8" },
-            { range: [5, 6], color: "#d0d0d0" },
-            { range: [6, 7], color: "#c8c8c8" },
-            { range: [7, 8], color: "#c0c0c0" },
-            { range: [8, 9], color: "#b8b8b8" },
-            // Add more range-color pairs here
-            { range: [9, 10], color: "#b0b0b0" },
-            { range: [10, 11], color: "#a8a8a8" },
-            // Add more range-color pairs as needed
-          ]
-        }
-      }
-    ];
-    const layout = {
-      width: 400,
-      height: 300,
-      margin: { t: 0, b: 0 }
-    };
-    Plotly.newPlot("gauge", data, layout);
-  }
-  
-  
-  // Function to display the sample metadata
-  function displayMetadata(selectedSubjectId, data) {
-    const selectedMetadata = data.metadata.find(metadata => metadata.id === parseInt(selectedSubjectId));
-    const metadataDiv = d3.select("#sample-metadata");
-    metadataDiv.html("");
-    Object.entries(selectedMetadata).forEach(([key, value]) => {
-      metadataDiv.append("p").text(`${key}: ${value}`);
+  const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+  d3.json(url)
+    .then(function (data) {
+      init(data);
+    })
+    .catch(function (error) {
+      console.log("Error loading data:", error);
     });
-    const washingFrequency = selectedMetadata.wfreq || 0;
-    createGaugeChart(washingFrequency);
-  }
+}
+
+// Function to initialize the dashboard
+function init(data) {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  var sampleNames = data.names;
+  sampleNames.forEach((sample) => {
+    selector.append("option").text(sample).property("value", sample);
+  });
+
+  // Use the first sample from the list to build the initial plots
+  var firstSample = sampleNames[0];
+  buildCharts(data, firstSample);
+  buildMetadata(data, firstSample);
+
+  // Add event listener for the dropdown menu
+  selector.on("change", function () {
+    var newSample = selector.property("value");
+    buildCharts(data, newSample);
+    buildMetadata(data, newSample);
+  });
+}
+
+// Call the function to read samples.json
+readSamplesJSON();
+
+// Function to handle changes in the dropdown menu
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  d3.json("samples.json").then((data) => {
+    buildCharts(data, newSample);
+    buildMetadata(data, newSample);
+  });
+}
+
+// Function to build the metadata panel
+function buildMetadata(data, sample) {
+  var metadata = data.metadata;
+  // Filter the data for the object with the desired sample number
+  var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  var result = resultArray[0];
+  // Use d3 to select the panel with id of `#sample-metadata`
+  var PANEL = d3.select("#sample-metadata");
+
+  // Use `.html("") to clear any existing metadata
+  PANEL.html("");
+
+  // Use `Object.entries` to add each key and value pair to the panel
   
-  // Function to handle changes in the dropdown menu
-  function optionChanged(selectedSubjectId, data) {
-    createBarChart(selectedSubjectId, data);
-    createBubbleChart(selectedSubjectId, data);
-    displayMetadata(selectedSubjectId, data);
-    
-  }
-  
+  Object.entries(result).forEach(([key, value]) => {
+    PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  });
+}
+
+// Function to build the charts
+function buildCharts(data, sample) {
+  var samplesArray = data.samples;
+  // Create a variable that filters the samples for the object with the desired sample number.
+  var selectedId = samplesArray.filter(data => data.id == sample);
+  var firstSample = selectedId[0];
+  var otuIds = firstSample.otu_ids;
+  var otuLabels = firstSample.otu_labels;
+  var sampleValues = firstSample.sample_values;
+
+  // Create the yticks for the bar chart.
+  var yticks = otuIds.slice(0, 10).map(id => "OTU " + id).reverse();
+
+  // Create the trace for the bar chart.
+  var barData = [{
+    x: sampleValues.slice(0, 10).reverse(),
+    text: otuLabels.slice(0, 10).reverse(),
+    type: "bar",
+    orientation: "h"
+  }];
+
+  // Create the layout for the bar chart.
+  var barLayout = {
+    title: "Top 10 Bacteria Strains",
+    yaxis: {
+      tickmode: "array",
+      tickvals: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      ticktext: yticks
+    },
+    annotations: [{
+      xref: 'paper',
+      yref: 'paper',
+      x: 0.5,
+      xanchor: 'center',
+      y: -0.25,
+      yanchor: 'center',
+      text: 'This chart shows the Top 10 most prominent strains of bacteria found in this sample',
+      showarrow: false
+    }]
+  };
+
+  // Use Plotly to plot the data with the layout.
+  Plotly.newPlot("bar", barData, barLayout, { responsive: true });
+
+  // Create Trace for bubble chart
+  var bubbleData = [{
+    x: otuIds,
+    y: sampleValues,
+    text: otuLabels,
+    mode: 'markers',
+    marker: {
+      size: sampleValues,
+      color: otuIds,
+      colorscale: "plasma"
+    }
+  }];
+
+  // Create the layout for the bubble chart.
+  var bubbleLayout = {
+    title: "Bacteria Cultures per Sample",
+    showlegend: false,
+    xaxis: { title: "OTU ID", automargin: true },
+    yaxis: { automargin: true },
+    hovermode: "closest"
+  };
+
+  // Use Plotly to plot the data with the layout.
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout, { responsive: true });
+
+  // Create a variable that filters metadata for chosen sample
+  var metadata_SelId = data.metadata.filter(data => data.id == sample);
+  var washFreq = +metadata_SelId[0].wfreq;
+
+  // Create the trace for the gauge chart.
+  var gaugeData = [{
+    domain: { x: [0, 1], y: [0, 1] },
+    value: washFreq,
+    title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per week" },
+    type: "indicator",
+    mode: "gauge+number",
+    gauge: {
+      axis: {
+        range: [null, 10],
+        tickmode: "array",
+        tickvals: [0, 2, 4, 6, 8, 10],
+        ticktext: [0, 2, 4, 6, 8, 10]
+      },
+      bar: { color: "black" },
+      steps: [
+        { range: [0, 2], color: "red" },
+        { range: [2, 4], color: "orange" },
+        { range: [4, 6], color: "yellow" },
+        { range: [6, 8], color: "lime" },
+        { range: [8, 10], color: "green" }]
+    }
+  }];
+
+  // Create the layout for the gauge chart.
+  var gaugeLayout = {
+    autosize: true,
+    annotations: [{
+      xref: 'paper',
+      yref: 'paper',
+      x: 0.5,
+      xanchor: 'center',
+      y: 0,
+      yanchor: 'center',
+      text: "This gauge shows the washing frequency of the selected sample.",
+      showarrow: false
+    }]
+  };
+
+  // Use Plotly to plot the gauge data and layout.
+  Plotly.newPlot("gauge", gaugeData, gaugeLayout, { responsive: true });
+}
